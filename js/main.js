2a1870cab55932f5cf2720bc9c9369418a8062d5
github <takuseifu@gmail.com>
2023-02-18 19:14:24 +0900

    Day1課題

--- js/main.js
+++ js/main.js
@@ -4,4 +4,48 @@ $(function($) {
         interval: 5000,
         loop: true
     });
-})
+
+    $(window) .scroll(function(){
+        var targetElement = $('.fadein').offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > targetElement - windowHeight + 200) {
+            $('.fadein').css('opacity', '1');
+            $('.fadein').css('transform', 'translateY(0)');
+        }
+    });
+
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        canterMode: true,
+        centerPadding: '20%',
+    });
+
+    $('.more-button').on('click',function(){
+        if ($('.open-box').is(':hidden')){
+            $('.open-box').slideDown();
+            $(this).text('close');
+            $(this).css('background-color','#808080');
+        }else{
+            $('.open-box').slideUp();
+            $(this).text('more');
+            $(this).css('background-color','#fabb51')
+        }
+    });
+
+    $(".modal-button").modaal();
+
+    $('nav a[href^="#"]').click(function(){
+        var adjust = 50;
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'href' : href);
+        var position = target.offset().top - adjust;
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+});
